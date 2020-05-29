d64d5f60d2539525f1a3b4cb85c17db4a214912f
Sherman Rose <s.rose@vtime.net>
2020-05-29 20:44:33 +0000

    GAME START

---
+++ Source/Discordant/DiscordantGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DiscordantGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DISCORDANT_API ADiscordantGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
